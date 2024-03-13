618d027a7bca809f83ec6f3e3159acebb7f89298
Joris Bertier <joris.bertier@gmail.com>
2024-03-13 11:11:48 -0500

    add component nav & styles components

---
+++ src/components/nav.jsx
@@ -0,0 +1,7 @@
+function Nav() {
+    return <nav>
+        <div>Test</div>
+    </nav>
+}
+
+export default Nav
--- src/pages/Home/App.jsx
+++ src/pages/Home/App.jsx
@@ -1,7 +1,10 @@
 import '../../../src/App.css'
+import Nav from '../../components/nav';
+
 function App() {
   return (
     <div className="App">
+      <Nav></Nav>
       <h1>Hello</h1>
     </div>
   );
