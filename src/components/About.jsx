524076c71b5afd78a0c8a8325a1fcf1eeced87e1
Joris Bertier <joris.bertier@gmail.com>
2024-03-18 16:56:42 -0500

    edit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -133,7 +133,23 @@ const TextAbout = styled.p `
     color: white;
     font-size: 0.8rem;
 `
+const SvgWrapper = styled.div `
+    height: 80px;
+    width: 80px;
+    border-radius: 20px;
+    background: blue;
+`
 
+const SvgBackground = styled.svg `
+margin-right: 20px;
+    background-color: #CBCBC8;
+    border-radius: 10px;
+    padding: 10px;
+    margin-top: -80px;
+    height: 3rem;
+    width: 3rem;
+
+`
 
 export const About = () => {
 
@@ -144,6 +160,9 @@ export const About = () => {
         let parisOffset = 1;
         let parisHours = (date.getUTCHours() + parisOffset) % 24;
         let parisMin = (date.getUTCMinutes());
+        if(parisMin < 10) {
+            parisMin = '0'+ parisMin;
+        }
         return {hours: parisHours, minutes: parisMin}
     }
 
@@ -156,7 +175,6 @@ export const About = () => {
     
     }, [])
 
-    console.log(typeof(parisHours));
   return (
 
     <ParentWrapperContact>
@@ -191,14 +209,23 @@ export const About = () => {
 
         <WrapperContact $20>
             <SectionContact $20 $grey>
-            <Subtitle>Aka : </Subtitle>
+                <Subtitle>Social Networks - </Subtitle><br/>
+                    <SvgBackground xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">
+                        <path d="M 24.929688 2.0019531 C 24.819813 2.0024531 24.709016 2.0206406 24.603516 2.0566406 L 3.671875 9.3417969 C 3.227875 9.4957969 2.9498125 9.93925 3.0078125 10.40625 L 6.3164062 37.529297 C 6.3554062 37.850297 6.5450781 38.133109 6.8300781 38.287109 L 24.478516 47.878906 C 24.626516 47.959906 24.791078 48 24.955078 48 C 25.120078 48 25.286547 47.958953 25.435547 47.876953 L 43.173828 38.154297 C 43.455828 37.999297 43.647547 37.717438 43.685547 37.398438 L 46.992188 10.277344 C 47.050187 9.8083437 46.769312 9.3609844 46.320312 9.2089844 L 25.253906 2.0527344 C 25.148906 2.0177344 25.039562 2.0014531 24.929688 2.0019531 z M 25 7 L 38 35 L 33.5625 35 L 30.78125 29.007812 L 19.21875 29.007812 L 16.4375 35 L 12 35 L 25 7 z M 25 16.5 L 21.080078 25 L 28.919922 25 L 25 16.5 z"></path>
+                    </SvgBackground>
+                    <SvgBackground xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">
+                        <path d="M 24.929688 2.0019531 C 24.819813 2.0024531 24.709016 2.0206406 24.603516 2.0566406 L 3.671875 9.3417969 C 3.227875 9.4957969 2.9498125 9.93925 3.0078125 10.40625 L 6.3164062 37.529297 C 6.3554062 37.850297 6.5450781 38.133109 6.8300781 38.287109 L 24.478516 47.878906 C 24.626516 47.959906 24.791078 48 24.955078 48 C 25.120078 48 25.286547 47.958953 25.435547 47.876953 L 43.173828 38.154297 C 43.455828 37.999297 43.647547 37.717438 43.685547 37.398438 L 46.992188 10.277344 C 47.050187 9.8083437 46.769312 9.3609844 46.320312 9.2089844 L 25.253906 2.0527344 C 25.148906 2.0177344 25.039562 2.0014531 24.929688 2.0019531 z M 25 7 L 38 35 L 33.5625 35 L 30.78125 29.007812 L 19.21875 29.007812 L 16.4375 35 L 12 35 L 25 7 z M 25 16.5 L 21.080078 25 L 28.919922 25 L 25 16.5 z"></path>
+                    </SvgBackground>
+                    <SvgBackground xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">
+                        <path d="M 24.929688 2.0019531 C 24.819813 2.0024531 24.709016 2.0206406 24.603516 2.0566406 L 3.671875 9.3417969 C 3.227875 9.4957969 2.9498125 9.93925 3.0078125 10.40625 L 6.3164062 37.529297 C 6.3554062 37.850297 6.5450781 38.133109 6.8300781 38.287109 L 24.478516 47.878906 C 24.626516 47.959906 24.791078 48 24.955078 48 C 25.120078 48 25.286547 47.958953 25.435547 47.876953 L 43.173828 38.154297 C 43.455828 37.999297 43.647547 37.717438 43.685547 37.398438 L 46.992188 10.277344 C 47.050187 9.8083437 46.769312 9.3609844 46.320312 9.2089844 L 25.253906 2.0527344 C 25.148906 2.0177344 25.039562 2.0014531 24.929688 2.0019531 z M 25 7 L 38 35 L 33.5625 35 L 30.78125 29.007812 L 19.21875 29.007812 L 16.4375 35 L 12 35 L 25 7 z M 25 16.5 L 21.080078 25 L 28.919922 25 L 25 16.5 z"></path>
+                    </SvgBackground>
             </SectionContact>
             <SectionContact $50>
                 
                     
                         <Subtitle>
-                        Futur <br/> Stack</Subtitle>
-
+                        Futur <br/> Projects -</Subtitle>
+                        <TextAbout>Dating website <br/> App Desktop manage stock</TextAbout>
                     
             </SectionContact>
             <SectionContact $30></SectionContact>
