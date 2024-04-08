efe80c40ba54571f371f461bf6e115c15ce31f07
Joris Bertier <joris.bertier@gmail.com>
2024-04-08 15:45:07 -0500

    crypt- encrypt

--- src/components/Encrypt/Encrypt.jsx
+++ src/components/Encrypt/Encrypt.jsx
@@ -15,26 +15,86 @@ const ResponseContainer = styled.div `
 export const Encrypt = () => {
     
     const [inputValue, setInputValue] = useState('')
-    const [submitedValue, setSubmitedValue] = useState('')
+    // const [submitedValue, setSubmitedValue] = useState('')
+    const [encryptValue, setEncryptValue] = useState('')
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const encryptData = (data) => {
+        let encryptedData = '';
+    
+        for (let i = 0; i < data.length; i++) {
+            const char = data[i];
+            const charCode = data.charCodeAt(i);
+            const shift = i + 1;
+    
+            const encryptedCharCode = charCode + shift;
+    
+            if (encryptedCharCode > 126) {
+                encryptedData += String.fromCharCode(32 + (encryptedCharCode - 126));
+            } else {
+                encryptedData += String.fromCharCode(encryptedCharCode);
+            }
+        }
+    
+        return encryptedData;
+    };
+    const decryptData = (encryptedData) => {
+        let decryptedData = '';
+    
+        for (let i = 0; i < encryptedData.length; i++) {
+            const char = encryptedData[i];
+            const charCode = encryptedData.charCodeAt(i);
+
+            const shift = i + 1;
+
+            let decryptedCharCode = charCode - shift;
+            while (decryptedCharCode < 32) {
+                decryptedCharCode += 95;
+            }
+
+            decryptedData += String.fromCharCode(decryptedCharCode);
+        }
+    
+        return decryptedData;
+    };
 
     const handleChange = (e) => {
         setInputValue(e.target.value);
     }
     const handleSubmit = (event) => {
         event.preventDefault();
-        setSubmitedValue(inputValue);
-        console.log(submitedValue)
+        const textEncrypted = encryptData(inputValue)
+        setEncryptValue(textEncrypted)
+        // ser
+        // setEncryptValue(encryptData(inputValue));
+        // console.log(textEncrypted)
     }
-    
+    const handleSubmitDecrypt = (event) => {
+        event.preventDefault();
+        const textEncrypted = decryptData(inputValue)
+        setEncryptValue(textEncrypted)
+    }
+
     return (
     <div>
+        {isOpen ? 
         <form onSubmit={handleSubmit}>
             <input type="text" onChange={handleChange}/>
-            <button type="submit">eNCRYPTER</button>
+            <button type="submit">Encrypter</button>
+            <button onClick={() => setIsOpen(false)}>change</button>
         </form>
+        :
+        <form onSubmit={handleSubmitDecrypt}>
+            <input type="text" onChange={handleChange}/>
+            <button type="submit">Decrypter</button>
+            <button onClick={() => setIsOpen(true)}>change</button>
+        </form>
+        }
         <div>
+            
             <ResponseArea></ResponseArea>
-            <ResponseContainer>{submitedValue}</ResponseContainer>
+            <ResponseContainer>{encryptValue}</ResponseContainer>
         </div>
     </div>
   )
