c5ccb92af8e7f6e2c6271494659dda606cdca0de
Joris Bertier <joris.bertier@gmail.com>
2024-04-08 17:34:34 -0500

    styled response wrapper

--- src/components/Encrypt/Encrypt.jsx
+++ src/components/Encrypt/Encrypt.jsx
@@ -1,30 +1,104 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import {Logos} from '../../datas/LogoDatas'
+
+const EncryptContainer = styled.div `
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+const WrapperResponse = styled.div `
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    // justify-content: center;
+    // text-align: center;
+`
+
 
 const ResponseArea = styled.div `
     background: linear-gradient(45deg, #9edbff, #b3a0ff 50%, #f29dff);
     border-radius: 20px 20px 0 0;
     padding: 10px 20px;
     // position: relative;
+    width: 70%;
+    margin-left: 15%;
+    margin-top: 20px;
 `
 const ResponseContainer = styled.div `
     height: 5rem;
     background: blue;
+    width: 70%;
+    margin-left: 15%;
+    padding: 10px 20px; 
+    border-radius: 0 0 20px 20px;
+`
+
+const InputEncrypt = styled.input `
+width: 70%;
+    border: none;
+    padding: 1rem;
+    border-radius: 1rem;
+    background: #e8e8e8;
+    // box-shadow: 20px 20px 60px #c5c5c5,
+        // -20px -20px 60px #ffffff;
+    transition: 0.3s;
+        width: 100px;
+        &:focus {
+            outline-color: #e8e8e8;
+            background: #e8e8e8;
+            box-shadow: inset 20px 20px 60px #c5c5c5,
+                inset -20px -20px 60px #ffffff;
+            transition: 0.3s;
+        }
+`
+
+const ButtonSubmit = styled.button `
+    padding: 10px;
+    background: teal;
+    border: none;
+    border-radius: 50%;
 
 `
+const LogoSend = styled.img `
+    width: 20px;
+    height: 20px;
+`
+
+const SwitchButton = styled.button`
+    position: relative;
+    padding: 5px 10px;
+    background-color: #eee;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    border-radius: 20px;
+`;
+
+const Indicator = styled.div`
+    position: absolute;
+    border-radius: 50%;
+    top: 0;
+    left: 0;
+    width: 50%;
+    height: 100%;
+    background-color: #ccc;
+    transition: transform 0.3s ease;
+    transform: ${({ encrypt }) => encrypt ? 'translateX(0)' : 'translateX(100%)'};
+`;
+
+
 export const Encrypt = () => {
     
     const [inputValue, setInputValue] = useState('')
-    // const [submitedValue, setSubmitedValue] = useState('')
     const [encryptValue, setEncryptValue] = useState('')
-
     const [isOpen, setIsOpen] = useState(false)
 
     const encryptData = (data) => {
         let encryptedData = '';
     
         for (let i = 0; i < data.length; i++) {
-            const char = data[i];
             const charCode = data.charCodeAt(i);
             const shift = i + 1;
     
@@ -43,9 +117,8 @@ export const Encrypt = () => {
         let decryptedData = '';
     
         for (let i = 0; i < encryptedData.length; i++) {
-            const char = encryptedData[i];
-            const charCode = encryptedData.charCodeAt(i);
 
+            const charCode = encryptedData.charCodeAt(i);
             const shift = i + 1;
 
             let decryptedCharCode = charCode - shift;
@@ -62,14 +135,13 @@ export const Encrypt = () => {
     const handleChange = (e) => {
         setInputValue(e.target.value);
     }
+
     const handleSubmit = (event) => {
         event.preventDefault();
         const textEncrypted = encryptData(inputValue)
         setEncryptValue(textEncrypted)
-        // ser
-        // setEncryptValue(encryptData(inputValue));
-        // console.log(textEncrypted)
     }
+
     const handleSubmitDecrypt = (event) => {
         event.preventDefault();
         const textEncrypted = decryptData(inputValue)
@@ -77,25 +149,19 @@ export const Encrypt = () => {
     }
 
     return (
-    <div>
-        {isOpen ? 
-        <form onSubmit={handleSubmit}>
-            <input type="text" onChange={handleChange}/>
-            <button type="submit">Encrypter</button>
-            <button onClick={() => setIsOpen(false)}>change</button>
-        </form>
-        :
-        <form onSubmit={handleSubmitDecrypt}>
-            <input type="text" onChange={handleChange}/>
-            <button type="submit">Decrypter</button>
-            <button onClick={() => setIsOpen(true)}>change</button>
-        </form>
-        }
-        <div>
-            
-            <ResponseArea></ResponseArea>
-            <ResponseContainer>{encryptValue}</ResponseContainer>
-        </div>
-    </div>
-  )
+        <EncryptContainer>
+            <form onSubmit={isOpen ? handleSubmitDecrypt : handleSubmit}>
+                <InputEncrypt type="text" onChange={handleChange}/>
+                <ButtonSubmit type="submit"><LogoSend src={Logos.Send} alt="logo send" /></ButtonSubmit>
+                <SwitchButton onClick={() => setIsOpen(!isOpen)}>
+                    <Indicator encrypt={isOpen} />
+                    {isOpen ? 'Decrypt' : 'Encrypt'}
+                </SwitchButton>
+            </form>
+            <WrapperResponse>
+                <ResponseArea></ResponseArea>
+                <ResponseContainer>{encryptValue}</ResponseContainer>
+            </WrapperResponse>
+        </EncryptContainer>
+    )
 }
